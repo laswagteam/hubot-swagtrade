8047e29d2147bf759b7fb346f7f7fadfbc2b9afe
agix <florian.gaultier@gmail.com>
2016-07-24 12:50:46 +0200

    Multiple transactions support

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function(robot) {
 
     var request = require('request'),
     fs = require('fs'),
-    api_url = 'https://btc-e.com/api/3/ticker/eth_usd';
+    api_url = 'https://btc-e.com/api/3/ticker/eth_usd-btc_usd';
 
     var config = JSON.parse(fs.readFileSync(process.env.SWAG_TRADE_CONFIG||'config.json'));
 
@@ -11,19 +11,37 @@ module.exports = function(robot) {
             if(!error && response.statusCode == 200) {
 
                 var data = JSON.parse(body);
-                var value = data.eth_usd.sell
-                var reply = '';
+                var eth = data.eth_usd.sell
+                var btc = data.btc_usd.sell
+                var reply = 'ETH = $ '+eth+'\n';
+                reply += 'BTC = $ '+btc+'\n';
 
                 config.forEach(function(mofo) {
-                    var init = mofo.amount*mofo.value;
-                    var now = mofo.amount*value;
-                    var win = Math.round((now-init)*100)/100;
-                    win += mofo.already;
-                    if(win >= 0){
-                        reply += mofo.name + ' : +$'+ win +'\n';
+                    var win = 0;
+                    mofo.transactions.forEach(function(transaction){
+                        var now;
+                        var init = transaction.amount*transaction.value;
+                        if(transaction.sold < 0){
+                            if(transaction.type === 'eth'){
+                                now = transaction.amount*eth;
+                            }
+                            else if(transaction.type === 'btc'){
+                                now = transaction.amount*btc;
+                            }
+                        }
+                        else{
+                           now = transaction.amount*transaction.sold;
+                        }
+
+                        win += (now-init);
+                    });
+                    var round_win = Math.round((win)*100)/100;
+
+                    if(round_win >= 0){
+                        reply += mofo.name + ' : +$'+ round_win +'\n';
                     }
                     else{
-                        reply += mofo.name + ' : -$'+ (-win) +'\n';
+                        reply += mofo.name + ' : -$'+ (-round_win) +'\n';
                     }
                 });
 

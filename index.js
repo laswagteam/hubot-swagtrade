1bf39aa6d8cfc76427ad961e1c3dd70046bc7f9b
Charley DAVID <calyhre@gmail.com>
2017-03-03 12:08:57 -0300

    Fix indent

--- index.js
+++ index.js
@@ -5,50 +5,50 @@ module.exports = function(robot) {
     api_url = 'https://btc-e.com/api/3/ticker/eth_usd-btc_usd';
 
     robot.respond(/(money)/i, function(msg) {
-        var config = JSON.parse(fs.readFileSync(process.env.SWAG_TRADE_CONFIG||'config.json'));
-        request(api_url, function (error, response, body) {
-            if(!error && response.statusCode == 200) {
+      var config = JSON.parse(fs.readFileSync(process.env.SWAG_TRADE_CONFIG||'config.json'));
+      request(api_url, function (error, response, body) {
+        if(!error && response.statusCode == 200) {
 
-                var data = JSON.parse(body);
+          var data = JSON.parse(body);
 
-                const price = {
-                    usd: 1,
-                    eth: data.eth_usd.sell,
-                    btc: data.btc_usd.sell,
-                };
+          const price = {
+            usd: 1,
+            eth: data.eth_usd.sell,
+            btc: data.btc_usd.sell,
+          };
 
-                let reply = 'ETH = $'+price.eth+'\n';
-                reply += 'BTC = $'+price.btc+'\n';
+          let reply = 'ETH = $'+price.eth+'\n';
+          reply += 'BTC = $'+price.btc+'\n';
 
-                config.forEach(function(mofo) {
-  
-                  const balance = { usd : 0, btc: 0, eth: 0 };
+          config.forEach(function(mofo) {
 
-                  mofo.transactions.forEach(function(transaction) {
-                    const factor = transaction.type === 'buy' ? 1 : -1;
-                    balance.usd = balance.usd + (-factor * transaction.volume * transaction.rate);
-                    balance[transaction.currency] = balance[transaction.currency] + (factor * transaction.volume);
-                  });
-                  
-                  let final_usd_balance = 0;
-                  Object.keys(balance).forEach((currency) => {
-                    final_usd_balance += balance[currency] * price[currency];
-                  })
+            const balance = { usd : 0, btc: 0, eth: 0 };
 
-                  final_usd_balance = Math.round((final_usd_balance)*100)/100;
+            mofo.transactions.forEach(function(transaction) {
+              const factor = transaction.type === 'buy' ? 1 : -1;
+              balance.usd = balance.usd + (-factor * transaction.volume * transaction.rate);
+              balance[transaction.currency] = balance[transaction.currency] + (factor * transaction.volume);
+            });
 
-                  if(final_usd_balance >= 0){
-                    reply += mofo.name + ' : +$'+ final_usd_balance +' :juppedealwithit:\n';
-                  }
-                  else{
-                    reply += mofo.name + ' : -$'+ (-final_usd_balance) +' :nelson:\n';
-                  }
-                });
+            let final_usd_balance = 0;
+            Object.keys(balance).forEach((currency) => {
+              final_usd_balance += balance[currency] * price[currency];
+            })
 
-                msg.send(reply);
-            } else {
-                msg.send(error);
+            final_usd_balance = Math.round((final_usd_balance)*100)/100;
+
+            if(final_usd_balance >= 0){
+              reply += mofo.name + ' : +$'+ final_usd_balance +' :juppedealwithit:\n';
             }
-        });
+            else{
+              reply += mofo.name + ' : -$'+ (-final_usd_balance) +' :nelson:\n';
+            }
+          });
+
+          msg.send(reply);
+        } else {
+          msg.send(error);
+        }
+      });
     });
 };
